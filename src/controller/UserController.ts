93152d999eab9575adc5189aef0870893ef10043
Isabela Fatima <isabelafatimadelimagx@gmail.com>
2022-11-06 14:38:20 -0300

    first commit

---
+++ src/controller/UserController.ts
@@ -0,0 +1,43 @@
+import {Request,Response} from "express"
+import User from "../database/schemas/User"
+
+class UserController{
+
+    async find(request: Request,response: Response){
+        try{
+            const users = await User.find();
+             return response.json(users)
+        }catch(error){
+            return response.status(500).json({
+                error: "Something wrong happened, try again",
+                message: error
+            })
+        }
+    }
+
+    async create(request: Request,response: Response){
+        const {name,email,password} = request.body;
+
+        try{
+            const UserExist = await User.findOne({email})
+            if(UserExist){
+                return response.status(400).json({
+                    error: "Error",
+                    Message: "User already exists"
+                })
+            }
+            const user = await User.create({
+                name,
+                email,
+                password
+            });
+            return response.json(user);
+        } catch(error){
+            return response.status(500).send({
+                error: 'Registration failed',
+                message: error
+            })
+        }
+    }
+}
+export default new UserController;
